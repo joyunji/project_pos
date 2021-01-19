5ee9deb873253636d97f8f00a127e7813484959e
dbtmx <dbtmx@LAPTOP-1PJ2GB3G>
2021-01-19 21:32:58 +0900

    카페, 현금 결제 추가

--- POS/src/client/Main.java
+++ POS/src/client/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Pos pos = new BookstorePos();
-		pos.setPaymentMethod(new CardPayment());
+		
+		pos.setPaymentMethod(new CardPayment());		
+		pos.pay();
+		System.out.println("--------------------------------------");
+		pos.setPaymentMethod(new KakaoPayment());
+		pos.pay();
+		System.out.println("--------------------------------------");
+		pos.setPaymentMethod(new CashPayment());
 		pos.pay();
 
 	}
